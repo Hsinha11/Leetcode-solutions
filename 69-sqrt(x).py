59f948ccdb25e2416b200b6cc23bcb6a06e649d1
Rishiwant1729 <rishiwamtk143@gmail.com>
2024-10-23 16:59:43 +0530

    Create 69-sqrt(x).py

---
+++ 69-sqrt(x).py
@@ -0,0 +1,9 @@
+"""Q. Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python."""
+
+class Solution(object):
+    def mySqrt(self, x):
+        return int(x**0.5)
