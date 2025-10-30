c74a5920b7914bfc0d1278afd4a122e809d57a05
Kavya Rambhia <kavyarambhia07@gmail.com>
2025-10-30 16:36:40 +0530

    Solution for 35-Search-insert-positions

---
+++ 35-Search-insert-positions/35-Search-insert-positions.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, r = nums.size()-1;
+        int ceil = INT_MAX;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                // ceil = min(ceil, mid);
+                r = mid-1;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+
+        return l;
+    }
+};
