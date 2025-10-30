ed5e2e458f075bf11fdcee8d5d0e2026f07c8c81
Kavya Rambhia <kavyarambhia07@gmail.com>
2025-10-30 16:28:15 +0530

    Solution for 24-Swap-nodes-in-pairs

---
+++ 24-Swap-nodes-in-pairs/24-Swap-nodes-in-pairs.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head == NULL || head -> next == NULL){
+            return head;
+        }
+            
+        ListNode* temp = head->next;
+        
+        head->next = swapPairs(head->next->next);
+        temp->next = head;
+        
+        return temp;
+    }
+};
