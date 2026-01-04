67ce2807d333394f44bff944629a2ec655df94b8
Harianant Sinha <156214180+Hsinha11@users.noreply.github.com>
2026-01-04 14:54:45 +0530

    Time: 50 ms (98.85%) | Memory: 30.3 MB (49.54%) - LeetSync

--- 4138-minimum-deletion-cost-to-make-all-characters-equal/minimum-deletion-cost-to-make-all-characters-equal.py
+++ 4138-minimum-deletion-cost-to-make-all-characters-equal/minimum-deletion-cost-to-make-all-characters-equal.py
@@ -1,13 +1,9 @@
 class Solution:
     def minCost(self, s: str, cost: List[int]) -> int:
-        d={}
-        for i in range(len(s)):
-            d[s[i]] = d.get(s[i],0)+cost[i]
-        # d = dict(sorted(d.items(),key = lambda x: x[1],reverse=True))
-        # print(d)
-        s = sum(d.values())
-        maxi = max(d.values())
-        return (s-maxi)
+        d=defaultdict(int)
+        for i,c in zip(s,cost):
+            d[i]+=c
+        return sum(d.values()) - max(d.values())
         
         
         
